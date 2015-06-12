07860804767b997cd91faaaf304804b3e7f1e835
Alexis Lawson <achayes@me.com>
2015-06-12 04:15:40 +0000

    Added a profile and management page with jquery tabs

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -279,8 +279,9 @@ if (document.URL.match(/\/album.html/)) {
 ;require.register("scripts/app", function(exports, require, module) {
 
 require("./landing");
-require('./collection');
- require('./album');
+require("./collection");
+require("./album");
+require("./profile");
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
@@ -378,6 +379,34 @@ $(document).ready(function() {
     $('.selling-points .point').hover(onHoverAction, offHoverAction);
 
 });
+
+});
+
+;require.register("scripts/profile", function(exports, require, module) {
+// holds the name of our tab button container for selection later in the function
+
+var tabsContainer = ".user-profile-tabs-container";
+
+var selectTabHandler = function(event) {
+   $tab = $(this);
+   $(tabsContainer + " li").removeClass('active');
+   $tab.parent().addClass('active');
+
+var selectedTabName = $tab.attr('href');
+   console.log(selectedTabName);
+
+   $(".tab-pane").addClass('hidden');
+   $(selectedTabName).removeClass('hidden');
+   event.preventDefault();
+ };
+
+ if (document.URL.match(/\/profile.html/)) {
+   $(document).ready(function() {
+     var $tabs = $(tabsContainer + " a");
+     $tabs.click(selectTabHandler);
+     $tabs[0].click();
+   });
+ }
 
 });
 
