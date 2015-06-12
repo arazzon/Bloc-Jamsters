e696f9ac56115ab2a3dfdda40fcbaeda616fd3a4
Alexis Lawson <achayes@me.com>
2015-06-12 15:19:30 +0000

    trying to get app.js to update in heroku

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -303,6 +303,7 @@ if (document.URL.match(/\/album.html/)) {
      '/images/album-placeholders/album-9.jpg',
    ];
 
+//extra challenge to make images shuffle when title is clicked
 function shuffle(o) {
         for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
        return o;
