5b3f076fcffeb7a9c6adb09a608e9270ce1a0a36
Lex Lawson <lexlawson@Alexiss-iMac.gateway>
2015-06-25 21:05:59 +1000

    Made next and previous buttons on player-bar work with album or song repeating.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -139,17 +139,22 @@ blocJams.service('SongPlayer', function() {
      pause: function() {
        this.playing = false;
      },
+    
      next: function() {
        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
        currentTrackIndex++;
+      
        if (currentTrackIndex >= this.currentAlbum.songs.length) {
          currentTrackIndex = 0;
        }
+      
        this.currentSong = this.currentAlbum.songs[currentTrackIndex];
      },
+    
      previous: function() {
        var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
        currentTrackIndex--;
+      
        if (currentTrackIndex < 0) {
          currentTrackIndex = this.currentAlbum.songs.length - 1;
        }
@@ -163,4 +168,208 @@ blocJams.service('SongPlayer', function() {
      }
    };
  });
+
+
+
+
+
+
+
+
+
+
+// require("./landing");
+// require("./collection");
+// require("./album");
+// require("./profile");
+
+// Example Album
+var albumPicasso = {
+ name: 'The Colors',
+ artist: 'Pablo Picasso',
+ label: 'Cubism',
+ year: '1881',
+ albumArtUrl: '/images/album-placeholder.png',
+ songs: [
+     { name: 'Blue', length: '4:26' },
+     { name: 'Green', length: '3:14' },
+     { name: 'Red', length: '5:01' },
+     { name: 'Pink', length: '3:21'},
+     { name: 'Magenta', length: '2:15'}
+   ]
+};
+
+blocJams = angular.module('BlocJams',['ui.router']);
+
+blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
+  $locationProvider.html5Mode(true);
+
+  $stateProvider.state('landing', {
+    url: '/',
+    controller: 'Landing.controller',
+    templateUrl: '/templates/landing.html'
+  });
+
+  $stateProvider.state('song', {
+    url: '/song',
+    templateUrl: '/templates/song.html'
+  });
+
+  $stateProvider.state('collection', {
+    url: '/collection',
+    controller: 'Collection.controller',
+    templateUrl: '/templates/collection.html'
+  });
+
+  $stateProvider.state('album', {
+    url: '/album',
+    controller: 'Album.controller',
+    templateUrl: '/templates/album.html'
+  });
+}]);
+
+blocJams.controller('Landing.controller', ['$scope', function($scope) {
+  $scope.titleText = "Bloc Jamsters";
+  $scope.subText = "Turn the music up!";
+
+  $scope.subTextClicked = function() {
+    $scope.subText += '!';
+  };
+
+  $scope.albumURLs = [
+    '/images/album-placeholders/album-1.jpg',
+    '/images/album-placeholders/album-2.jpg',
+    '/images/album-placeholders/album-3.jpg',
+    '/images/album-placeholders/album-4.jpg',
+    '/images/album-placeholders/album-5.jpg',
+    '/images/album-placeholders/album-6.jpg',
+    '/images/album-placeholders/album-7.jpg',
+    '/images/album-placeholders/album-8.jpg',
+    '/images/album-placeholders/album-9.jpg',
+  ];
+
+  $scope.shuffle = function(o){ 
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  };
+
+  $scope.titleTextClicked = function() {
+    $scope.shuffle($scope.albumURLs);
+  };
+}]);
+
+blocJams.controller('Collection.controller', ['$scope', function($scope) {
+  $scope.hideOverlay = true;
+  $scope.albums = [];
+  for (var i = 0; i < 33; i++) {
+    $scope.albums.push(angular.copy(albumPicasso));
+  } 
+}]);
+
+blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+  $scope.album = angular.copy(albumPicasso);
+
+  var hoveredSong = null;
+
+  $scope.onHoverSong = function(song) {
+    hoveredSong = song;
+  };
+
+  $scope.offHoverSong = function(song) {
+    hoveredSong = null;
+  };
+
+  $scope.getSongState = function(song) {
+      if (song === SongPlayer.currentSong && SongPlayer.playing) {
+      return 'playing';
+    }
+    else if (song === hoveredSong) {
+      return 'hovered';
+    }
+    return 'default';
+  };
+
+  $scope.playSong = function(song) {
+    SongPlayer.setSong($scope.album, song);
+     SongPlayer.play();
+  };
+
+  $scope.pauseSong = function(song) {
+   SongPlayer.pause();
+  };
+}]);
+
+blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+   $scope.songPlayer = SongPlayer;
+ }]);
+
+blocJams.service('SongPlayer', function() {
+ 
+  var trackIndex = function(album, song) {
+     return album.songs.indexOf(song);
+   };
+ 
+   return {
+     currentSong: null,
+     currentAlbum: null,
+     playing: false,
+ 
+     play: function() {
+       this.playing = true;
+     },
+    
+     pause: function() {
+       this.playing = false;
+     },
+    
+next: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+ 
+       if ((currentTrackIndex >= this.currentAlbum.songs.length) || (currentTrackIndex < 0)) {
+ //        currentTrackIndex = 0;
+          this.currentSong = null;
+          this.playing = false;
+       }
+ 
+       else{
+          currentTrackIndex++;
+          this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+       }
+
+       var song = this.currentAlbum.songs[currentTrackIndex];
+       this.setSong(this.currentAlbum, song);   
+       
+       this.isNextForward = setFowardButton(currentTrackIndex, this.currentAlbum.songs.length);
+       this.isNextBackward = setBackwardButton(currentTrackIndex);
+     },
+    
+     previous: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+       currentTrackIndex--;
+      
+       if (currentTrackIndex < 0) {
+         //currentTrackIndex = this.currentAlbum.songs.length - 1;
+         //currentTrackIndex = 0;
+         this.currentSong = null;
+       }
+      
+       else{
+         this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+       }
+
+       this.isNextForward = setFowardButton(currentTrackIndex);
+       this.isNextBackward = setBackwardButton(currentTrackIndex);
+
+       var song = this.currentAlbum.songs[currentTrackIndex];
+       this.setSong(this.currentAlbum, song);
+
+     },
+    
+     setSong: function(album, song) {
+       this.currentAlbum = album;
+       this.currentSong = song;
+     }
+   };
+ });
+
 
